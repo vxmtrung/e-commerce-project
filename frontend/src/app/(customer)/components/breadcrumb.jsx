ec9172a51971044e30ace22c46268d1795e7bdc8
phuc-dang <phuc.dangphanminh@hcmut.edu.vn>
2025-04-19 10:06:24 +0700

    chore(customerLayout): update breadcrumb for /cart, /product/ and /product/detail page

--- frontend/src/app/(customer)/components/breadcrumb.jsx
+++ frontend/src/app/(customer)/components/breadcrumb.jsx
@@ -2,34 +2,39 @@
 import { Breadcrumb } from 'antd';
 import { HomeOutlined } from '@ant-design/icons';
 import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
-// Define display name for each path
 const breadcrumbMap = {
-  '/': 'Home',
-  '/profile': 'Profile',
-  '/cart': 'Cart',
-  '/product': 'Product',
-  '/checkout': 'Checkout',
-  '/about': 'About',
-  '/policy': 'Privacy Policy',
-  '/contact': 'Contact',
-  '/category': 'Category',
+  '/': 'Trang chủ',
+  '/cart': 'Giỏ hàng',
+  '/product': 'Sản phẩm',
+  '/product/detail': 'Chi tiết sản phẩm',
 };
 
 export default function BreadcrumbNav() {
   const pathname = usePathname();
   const pathSegments = pathname.split('/').filter((segment) => segment);
 
+  const breadcrumbItems = pathSegments.map((segment, index) => {
+    const url = `/${pathSegments.slice(0, index + 1).join('/')}`;
+    const name = breadcrumbMap[url] || segment;
+
+    return (
+      <Breadcrumb.Item key={url}>
+        <Link href={url}>{name}</Link>
+      </Breadcrumb.Item>
+    );
+  });
+
   return (
     <div style={{ padding: '10px 24px' }}>
       <Breadcrumb>
-        <Breadcrumb.Item href="/">
-          <HomeOutlined /> <span>Home</span>
+        <Breadcrumb.Item>
+          <Link href="/">
+            <HomeOutlined /> <span>Trang chủ</span>
+          </Link>
         </Breadcrumb.Item>
-        {pathSegments.map((segment, index) => {
-          const url = `/${pathSegments.slice(0, index + 1).join('/')}`;
-          return <Breadcrumb.Item key={url}>{breadcrumbMap[url] || segment}</Breadcrumb.Item>;
-        })}
+        {breadcrumbItems}
       </Breadcrumb>
     </div>
   );
