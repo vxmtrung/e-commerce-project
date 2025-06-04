f581531689fa3f35d933eb1b153c6b7fa8b29747
cuongnguyen2323 <cuong.nguyenmsmsms23@hcmut.edu.vn>
2025-06-04 22:05:39 +0700

    fix choosing product instance

--- frontend/src/app/(customer)/(about)/product/detail/page.jsx
+++ frontend/src/app/(customer)/(about)/product/detail/page.jsx
@@ -64,7 +64,7 @@ export default function DetailPage() {
 
   const options = productInstances.map((item) => ({
     id: item.id,
-    name: item.name.substring(10),
+    name: item.name,
   }));
   console.log('options: ', options);
 
