d2850200c2f7d550455f3ae754f4ab91bc50f072
cuongnguyen2323 <cuong.nguyenmsmsms23@hcmut.edu.vn>
2025-06-04 10:40:56 +0700

    update customer's detail page

--- frontend/src/app/(customer)/(about)/product/detail/page.jsx
+++ frontend/src/app/(customer)/(about)/product/detail/page.jsx
@@ -9,53 +9,73 @@ import { useSearchParams } from 'next/navigation';
 
 export default function DetailPage() {
   const id = useSearchParams().get('id');
-  const name = useSearchParams().get('name');
-  //   const brand = JSON.parse(localStorage.getItem(id)).brand || 'Idk Brand';
-  //   const brand = 'idk';
-  //   const img =
-  //     'https://media.hasaki.vn/wysiwyg/HaNguyen/nuoc-hoa-hong-klairs-khong-mui-cho-da-nhay-cam-180ml-1.jpg';
-  //   console.log(brand);
-  //   console.log(img);
+  const [product, setProduct] = useState({});
+  const [productInstances, setProductInstances] = useState([]);
+  const [brand, setBrand] = useState('');
 
+  const [instance, setInstance] = useState(null);
   useEffect(() => {
-    fetchProductDetail();
+    fetchProduct();
+    fetchProductInstances();
   }, []);
 
-  const [productDetail, setProductDetail] = useState([]);
-
-  const fetchProductDetail = async () => {
+  const fetchProduct = async () => {
     try {
-      const productDetailResponse = await fetch(
-        `http://localhost:3000/product-instances/${id}/detail`
+      const productResponse = await fetch(
+        `http://localhost:3000/products/${id}/detail`
       );
-      const detail = await productDetailResponse.json();
+      const productJson = await productResponse.json();
+      setProduct(productJson);
+      console.log('product: ', productJson);
 
-      const parentProductResponse = await fetch(
-        `http://localhost:3000/products/search?page=0&size=1&filter=name:eq:${name}`
-      );
-      const parentProduct = await parentProductResponse.json();
-
-      detail.productName = name;
-      detail.brand = parentProduct.items[0].brand.name || 'Unknown Brand';
-      detail.img =
-        parentProduct.items[0].productInstances.find((item) => item.id == id)
-          .productImgs[0]?.link ||
-        'https://media.hasaki.vn/wysiwyg/HaNguyen/nuoc-hoa-hong-klairs-khong-mui-cho-da-nhay-cam-180ml-1.jpg';
-      detail.shortDescription = parentProduct.items[0].description;
-      detail.longDescription = detail.description || detail.shortDescription;
-
-      setProductDetail(detail);
-      console.log(detail);
+      fetchBrand(productJson.brandId);
     } catch (error) {
-      console.error('Error fetching product detail:', error);
-      setProductDetail([]);
+      console.error('Error fetching product: ', error);
+      setProduct({});
     }
   };
 
+  const fetchProductInstances = async () => {
+    try {
+      const productInstancesResponse = await fetch(
+        `http://localhost:3000/product-instances?product-id=${id}`
+      );
+      const productInstancesJson = await productInstancesResponse.json();
+      setProductInstances(productInstancesJson);
+      console.log('product instances: ', productInstancesJson);
+    } catch (error) {
+      console.error('Error fetching product instances: ', error);
+      setProductInstances([]);
+    }
+  };
+
+  const fetchBrand = async (brandId) => {
+    try {
+      const brandsResponse = await fetch('http://localhost:3000/brands');
+      const brandsJson = await brandsResponse.json();
+      const brandName = brandsJson.find((item) => item.id == brandId)?.name;
+      setBrand(brandName || '');
+      console.log('brand: ', brandName);
+    } catch (error) {
+      console.error('Error fetching brand: ', error);
+      setBrand('');
+    }
+  };
+
+  const options = productInstances.map((item) => ({
+    id: item.id,
+    name: item.name.substring(10),
+  }));
+  console.log('options: ', options);
+
   return (
     <Space direction="vertical">
-      <ProductCard productDetail={productDetail} />
-      <ProductDetailMenu productDetail={productDetail} />
+      <ProductCard
+        options={options}
+        product={product}
+        productInstances={productInstances}
+        brand={brand}
+      />
       <ReviewSection />
     </Space>
   );
