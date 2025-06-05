f231c98fcce4b74d917a351bad9af702fdb3a5f9
vxmtrung <vuxuanmaitrung1821@gmail.com>
2025-06-05 10:22:37 +0700

    add data

---
+++ data/product_img.sql
@@ -0,0 +1,9 @@
+INSERT INTO "product_img" ("product_instance_id", "link", "status") VALUES 
+('261b4661-dbb1-4699-b42b-88fd131a25d2', 'https://bizweb.dktcdn.net/100/464/734/products/mascara-browit-nongchat-1663211938115.jpg', 't'),
+('cb2e50d3-e7d6-4a28-aea2-3916ffd15f7e', 'https://product.hstatic.net/200000701461/product/sg-11134201-7rbl2-lphknifcjmzw00_ce72f87b4b654cb4a1fbe3e3a9af9955_1024x1024.jpg', 't'),
+('a5f30ea2-3955-476c-b863-f3005949f093', 'https://cdn.thegioididong.com/Products/Images/6562/266356/serum-vitamin-c-balance-sang-da-30ml-1.jpg', 't'),
+('80f155ca-2a9d-48a7-9e51-34a8e3422367', 'https://myphamthuanchay.com/images/product/thach-hoa-hong-duong-am-cocoon-30ml-rose-aqua-gel-cream.jpg', 't'),
+('1f20b791-e31c-47dc-8fea-a071613b876d', 'https://lanchi.vn/wp-content/uploads/2021/11/DAU-GOI-TRESEMME-SALON-REBOND-640G.jpg', 't'),
+('56f7d7db-389c-4701-8158-51f146b2b491', 'https://media.hcdn.vn/catalog/product/g/o/google-shopping-xit-duong-chan-toc-aromatica-chiet-xuat-huong-thao-100ml-1692420869.jpg', 't'),
+('d905baf5-3114-4f63-8cb4-a1797000c6b1', 'https://product.hstatic.net/1000025647/product/3ce-kem-child-like.png', 't'),
+('dbccabdf-d800-48fd-9d75-68d2873da933', 'https://media.hcdn.vn/catalog/product/g/o/google-shopping-phan-nuoc-aperire-chong-nang-che-phu-cao-02-tong-tu-nhien-13g-1746691631.jpg', 't');
